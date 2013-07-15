12fe13115b0367789498a21e8d5b502ad5003ff1
Ipstenu <Ipstenu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-15 17:51:40 +0000

    Readme update for PHP 5.2 and WP 3.6
    git-svn-id: https://plugins.svn.wordpress.org/dreamobjects/trunk@741212 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/dhdo.php
+++ lib/dhdo.php
@@ -204,7 +204,7 @@ class DHDO {
 
         // All me files!
         if ( in_array('files', $sections) ) {
-            $backups = array_merge($backups, DHDO::rscandir(WP_CONTENT_DIR));
+            $backups = array_merge($backups, DHDO::rscandir(WP_CONTENT_DIR.'/uploads'));
             DHDO::logger('List of files added to the zip.');
         } 
         
